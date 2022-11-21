19ddc7b476d8a7b155aa832bfd3aa648b5ed6a61
Steve Calla <callasteven@gmail.com>
2022-11-21 14:44:00 -0700

    Add src folder

---
+++ client/src/components/ProductList/index.js
@@ -0,0 +1,84 @@
+import React, { useEffect } from 'react';
+import ProductItem from '../ProductItem';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+import spinner from '../../assets/spinner.gif';
+
+//section
+import { useSelector, useDispatch } from 'react-redux';
+import { useReducer } from 'react';
+import reducer from '../../utils/reducers';
+//section end
+
+function ProductList() {
+  // const [state, dispatch] = useStoreContext();
+
+  // const { currentCategory } = state;
+
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  // console.log('product query = ', data)
+
+  //section
+  const { currentCategory } = useSelector(state => state);
+  const { products } = useSelector(state => state);
+  let dispatch = useDispatch();
+  // console.log(currentCategory);
+  //section end
+
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
+    }
+  }, [data, loading, dispatch]);
+
+  function filterProducts() {
+    if (!currentCategory) {
+      return products;
+    }
+
+    return products.filter(
+      (product) => product.category._id === currentCategory
+    );
+  }
+
+  return (
+    <div className="my-2">
+      <h2>Our Products:</h2>
+      {products.length ? (
+        <div className="flex-row">
+          {filterProducts().map((product, index) => (
+            <ProductItem
+              key={product._id}
+              _id={product._id}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+              quantity={product.quantity}
+            />
+          ))}
+        </div>
+      ) : (
+        <h3>You haven't added any products yet!</h3>
+      )}
+      {loading ? <img src={spinner} alt="loading" /> : null}
+    </div>
+  );
+}
+
+export default ProductList;
