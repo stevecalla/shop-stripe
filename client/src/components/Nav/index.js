b1b07e8211bd7a05478722a09b8e9062555aa0d4
Steve Calla <callasteven@gmail.com>
2022-11-23 09:17:07 -0700

    Adjust urls

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,7 +8,7 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/#orderHistory">Order History</Link>
+            <Link to="/orderHistory">Order History</Link>
           </li>
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
@@ -22,10 +22,10 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/#signup">Signup</Link>
+            <Link to="/signup">Signup</Link>
           </li>
           <li className="mx-1">
-            <Link to="/#login">Login</Link>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       );
--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -32,7 +32,7 @@ function OrderHistory() {
                 <div className="flex-row">
                   {order.products.map(({ _id, image, name, price }, index) => (
                     <div key={index} className="card px-1 py-1">
-                      <Link to={`/#products/${_id}`}>
+                      <Link to={`/products/${_id}`}>
                         <img alt={name} src={`/images/${image}`} />
                         <p>{name}</p>
                       </Link>
